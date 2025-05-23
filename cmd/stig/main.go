4b526c63fa37ccd6a75b678d0dcf1a93d1239366
Nizar <nizarmah@hotmail.com>
2025-05-23 03:57:58 +0300

    Reset the game programmatically

--- cmd/stig/main.go
+++ cmd/stig/main.go
@@ -10,9 +10,9 @@ import (
 	"time"
 
 	"github.com/go-rod/rod"
-	"github.com/go-rod/rod/lib/proto"
 
 	"github.com/nizarmah/stig/internal/env"
+	"github.com/nizarmah/stig/internal/menu"
 )
 
 func main() {
@@ -34,15 +34,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer browser.Close()
+	// defer browser.Close()
 
-	// Start the game.
-	page, err := startGame(ctx, browser, e.GameURL, 10*time.Second)
+	// Open the game and its menu.
+	menu, err := menu.NewManager(ctx, browser, e.GameURL, 10*time.Second)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer page.Close()
+	defer menu.Close()
 
+	// Start the game.
+	if err := menu.StartGame(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Wait for the game to start.
+	time.Sleep(5 * time.Second)
+
+	// Reset the game.
+	if err := menu.ResetGame(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Wait for the game to reset.
 	time.Sleep(5 * time.Second)
 }
 
@@ -58,38 +72,4 @@ func connectToBrowser(
 	}
 
 	return browser, nil
-}
-
-func startGame(
-	ctx context.Context,
-	browser *rod.Browser,
-	gameURL string,
-	timeout time.Duration,
-) (*rod.Page, error) {
-	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
-	// Open the game page.
-	page, err := browser.Page(proto.TargetCreateTarget{URL: gameURL})
-	if err != nil {
-		return nil, fmt.Errorf("failed to create page: %w", err)
-	}
-
-	// Wait for the page to load.
-	if err := page.Context(ctx).WaitLoad(); err != nil {
-		return nil, fmt.Errorf("failed to wait for page to load: %w", err)
-	}
-
-	// Search for the "Start" button.
-	startButton, err := page.ElementX(`//span[text()="Start"]`)
-	if err != nil {
-		return nil, fmt.Errorf("failed to find start button: %w", err)
-	}
-
-	// Click on the "Start" button.
-	if err := startButton.Click(proto.InputMouseButtonLeft, 1); err != nil {
-		return nil, fmt.Errorf("failed to click on start button: %w", err)
-	}
-
-	return page, nil
 }
---
+++ internal/menu/menu.go
@@ -0,0 +1,97 @@
+// Package menu provides functions to interact with the menu.
+package menu
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/input"
+	"github.com/go-rod/rod/lib/proto"
+)
+
+// Manager manages the game menu.
+type Manager struct {
+	page *rod.Page
+}
+
+// NewManager creates a new manager.
+func NewManager(
+	ctx context.Context,
+	browser *rod.Browser,
+	gameURL string,
+	timeout time.Duration,
+) (*Manager, error) {
+	page, err := openInitialMenu(ctx, browser, gameURL, timeout)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create manager: %w", err)
+	}
+
+	return &Manager{page: page}, nil
+}
+
+// StartGame starts the game.
+func (m *Manager) StartGame() error {
+	// Search for the "Start" button.
+	startButton, err := m.page.ElementX(`//span[text()="Start"]`)
+	if err != nil {
+		return fmt.Errorf("failed to find start button: %w", err)
+	}
+
+	// Click on the "Start" button.
+	if err := startButton.Click(proto.InputMouseButtonLeft, 1); err != nil {
+		return fmt.Errorf("failed to click on start button: %w", err)
+	}
+
+	return nil
+}
+
+// ResetGame resets the game.
+func (m *Manager) ResetGame() error {
+	// Press "Escape" to open the initial menu again.
+	pressEscape := m.page.KeyActions().Type(input.Escape)
+	if err := pressEscape.Do(); err != nil {
+		return fmt.Errorf("failed to press escape: %w", err)
+	}
+
+	// Start the game.
+	if err := m.StartGame(); err != nil {
+		return fmt.Errorf("failed to reset game: %w", err)
+	}
+
+	return nil
+}
+
+// Close closes the manager and the game page.
+func (m *Manager) Close() error {
+	if err := m.page.Close(); err != nil {
+		return fmt.Errorf("failed to close page: %w", err)
+	}
+
+	return nil
+}
+
+// openInitialMenu opens the game initial menu.
+func openInitialMenu(
+	ctx context.Context,
+	browser *rod.Browser,
+	gameURL string,
+	timeout time.Duration,
+) (*rod.Page, error) {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	// Open the game page.
+	page, err := browser.Page(proto.TargetCreateTarget{URL: gameURL})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create page: %w", err)
+	}
+
+	// Wait for the page to load.
+	if err := page.Context(ctx).WaitLoad(); err != nil {
+		return nil, fmt.Errorf("failed to wait for page to load: %w", err)
+	}
+
+	return page, nil
+}
